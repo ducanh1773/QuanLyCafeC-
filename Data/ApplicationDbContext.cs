82b6bf8a8f112ac4567eb2aee9e09ee69ebfd9f6
unknown <anhductrannguyen170703@gmail.com>
2025-02-28 10:45:14 +0700

    main

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using QuanLyCafe.Model;
+
+namespace QuanLyCafe.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Account> Products { get; set; }
+    }
+}
---
+++ Model/Account.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuanLyCafe.Model
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string username { get; set; }
+        public string email { get; set; }
+        public string phoneNumber { get; set; }
+        public string address { get; set; }
+        public string password { get; set; }
+        public DateTime creat_at { get; set; }
+        public bool status { get; set; }
+        public bool deleted { get; set; }
+    }
+}
